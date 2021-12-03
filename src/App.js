1f2421a4418b363b15140ddcd266ee9c25f933f5
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-12-03 19:16:10 +0700

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      namaResto: "Codepolitan"
+      namaResto: "Codepolitan",
+      menuMakanan: [
+        {nama: "Ayam Bakar", harga: 25000, stock: 10},
+        {nama: "Nasi Goreng", harga: 25000, stock: 0}
+      ]
     };
   }
 
@@ -21,10 +25,18 @@ class App extends Component {
     return(
       <div className="App">
       <h1>Cafe and Resto {this.state.namaResto}</h1>
-      <button onClick= {() => this.handleGantiNama("Cipedes")}>Ganti nama resto</button>
+      <button className="btn btn-primary" onClick= {() => this.handleGantiNama("Cipedes")}>Ganti nama resto</button>
       <h2>Menu Makanan</h2>
-      <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={25000}/>
-      <MenuMakanan namaMenu={"Nasi Goreng"} hargaMenu={10000}/>
+      <MenuMakanan 
+        namaMenu = {this.state.menuMakanan[0].nama}
+        hargaMenu = {this.state.menuMakanan[0].harga}
+        stokMenu = {this.state.menuMakanan[0].stock}
+      />
+      <MenuMakanan 
+        namaMenu = {this.state.menuMakanan[1].nama}
+        hargaMenu = {this.state.menuMakanan[1].harga}
+        stokMenu = {this.state.menuMakanan[1].stock}
+      />
       <h2>Menu Minuman</h2>
       <MenuMinuman namaMenu={"Juice Melon"} hargaMenu={30000}/>
       <MenuMinuman namaMenu={"Es Krim"} hargaMenu={10000}/>
--- src/components/MenuMakanan/MenuMakanan.js
+++ src/components/MenuMakanan/MenuMakanan.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 let menuMakanan = (props) => {
+  if(props.stokMenu !== 0 ){
     return (
-    <div style={{ border: "1px solid black", width: 300, margin: "auto"}}>
-      <p>Nama menu : {props.namaMenu}</p>
-      <p>Harga : Rp {props.hargaMenu}</p>
-    </div>
-    )
+      <div style={{ border: "1px solid black", width: 300, margin: "auto"}}>
+        <p>Nama menu : {props.namaMenu}</p>
+        <p>Harga : Rp {props.hargaMenu}</p>
+        <p>Stock : {props.stokMenu}</p>
+      </div>
+      )
+  } else {
+    return null;
+  }
 }
 
 export default menuMakanan;
